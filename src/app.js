3b0795796d76752e93f8aeba9b3eb4a1de02d013
michelmarq1 <marquezmichel2003@gmail.com>
2025-04-01 22:58:07 +0000

    Excuse Generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 import "bootstrap";
 import "./style.css";
 
+let who = ['A two-headed octopus', 'My magical dog', 'An angry elephant', 'The angry ghost'];
+let action = ['yelled at', 'stole', 'threw', 'hid'];
+let what = ['my pizza', 'my shoes', 'my homework', 'my bottle of water'];
+let where = ['at the gym', 'at the park', 'at the cinema', 'in the backyard'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function generateExcuse() {
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+  let randomAction = action[Math.floor(Math.random() * action.length)];
+  let randomWhat = what[Math.floor(Math.random() * what.length)];
+  let randomWhere = where[Math.floor(Math.random() * where.length)];
+  return `${randomWho} ${randomAction} ${randomWhat} ${randomWhere}.`;
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+  document.getElementById("intro").innerHTML = "OMG! You will not believe me but...";
+  document.getElementById("excuse").innerHTML = generateExcuse();
 };
